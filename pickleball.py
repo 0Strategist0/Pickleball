c2d07de79d4c3f47cf84a482ecc6c187ed5aa801
0Strategist0 <0strategist0@gmail.com>
2024-08-30 15:41:46 -0700

    Made histogram plot

--- pickleball.py
+++ pickleball.py
@@ -3,56 +3,23 @@ import numpy.typing as npt
 import scipy.integrate as si
 import scipy.optimize as so
 import matplotlib.pyplot as plt
-from mayavi import mlab
+# from mayavi import mlab
+from tqdm import tqdm
 
 def main() -> None:
     # Define constants
-    NUM = {"x": 0, "vx": 1, "y": 2, "vy": 3}
-    NAMES = ("x position (m)", "x velocity (m/s)", "y position (m)", "y velocity (m/s)")
     T_MIN = 0.0
     T_MAX = 10.0
     DRAG_COEF = 0.5 * (1.2) * (37.0 * 10.0 ** (-3.0)) ** 2.0 * np.pi * 0.6 / (24.0 * 10 ** (-3.0)) # 0.5 * rho * A * CD / m
     INITIAL_SPEED_GUESS = 10.0
-    COURT_LENGTH = 13.4112
+    COURT_LENGTH = f2m(44.0)
     GRAVITY = 9.81
+    N_RANDOM_SAMPLES = 1000
     
     print(f"Drag Coefficient: {DRAG_COEF} 1/m")
     
-    # Define input variables
-    wind = 0.0
-    # x0 = 0.0 # in (0, 4.572)
-    y0 = 1.0 # in (0, 2.1336)
-    # angle = 0.5 * np.pi / 4.0
-    opponent = 10.0 # Between 8.8392 and 13.4112
-    
-    # Solve the system 
-    # print(solve_system(0.0, 1.0, y0, opponent, T_MIN, T_MAX, DRAG_COEF, wind, GRAVITY, COURT_LENGTH, INITIAL_SPEED_GUESS).t_events[1][0])
-    
-    GRID_SHAPE = (15, 15)
-    x0s, angles = np.meshgrid(np.linspace(0, 4.572, GRID_SHAPE[0]), np.linspace(0.0, np.pi / 4.0, GRID_SHAPE[1]))
-    x0s = x0s.T
-    angles = angles.T
-    # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    
-    for wind, color, label in zip((-5.0, 0.0, 5.0), ((1.0, 0.0, 0.0), (0.5,) * 3, (0.0, 0.0, 1.0)), ("Against the Wind", "No Wind", "With the Wind")):
-        times_to_opponent = np.asarray([solve_system(x0, 
-                                                    angle, 
-                                                    y0, 
-                                                    opponent, 
-                                                    T_MIN, 
-                                                    T_MAX, 
-                                                    DRAG_COEF, 
-                                                    wind, 
-                                                    GRAVITY, 
-                                                    COURT_LENGTH, 
-                                                    INITIAL_SPEED_GUESS).t_events[1][0] 
-                                        for x0, angle in zip(x0s.ravel(), angles.ravel())]).reshape(GRID_SHAPE)
-        
-        # Plot 3D
-        mlab.surf(x0s, angles, times_to_opponent, color=color)
-        # surface = ax.plot_surface(x0s, angles, times_to_opponent, color=color, label=label)
-    
-    mlab.show()
+    # Plot the time difference histogram
+    time_difference_plot(N_RANDOM_SAMPLES, T_MIN, T_MAX, DRAG_COEF, GRAVITY, COURT_LENGTH, INITIAL_SPEED_GUESS)
     
     # ax.set_xlabel("Initial Position (m)")
     # ax.set_ylabel("Launch Angle (rad)")
@@ -103,8 +70,102 @@ def main() -> None:
     # plt.ylabel("Ball Speed Relative to Air (m/s)")
     # plt.title(f"Ball Speed Relative to Air Over Time (Wind = {wind:.0f} m/s)")
     # plt.show()
+    
+        # Solve the system 
+    # print(solve_system(0.0, 1.0, y0, opponent, T_MIN, T_MAX, DRAG_COEF, wind, GRAVITY, COURT_LENGTH, INITIAL_SPEED_GUESS).t_events[1][0])
+    
+    # GRID_SHAPE = (15, 15)
+    # x0s, angles = np.meshgrid(np.linspace(0, 4.572, GRID_SHAPE[0]), np.linspace(0.0, np.pi / 4.0, GRID_SHAPE[1]))
+    # # x0s, angles = np.meshgrid(np.linspace(0, 4.572, GRID_SHAPE[0]), np.linspace(8.8392, 13.4112, GRID_SHAPE[1]))
+    # x0s = x0s.T
+    # angles = angles.T
+    # # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    # figure = mlab.figure("Time to Opponent")
+    
+    # for wind, color, label in zip((-5.0, 0.0, 5.0), ((1.0, 0.0, 0.0), (0.5,) * 3, (0.0, 0.0, 1.0)), ("Against the Wind", "No Wind", "With the Wind")):
+    #     times_to_opponent = np.asarray([solve_system(x0, 
+    #                                                 angle, 
+    #                                                 y0, 
+    #                                                 opponent, 
+    #                                                 T_MIN, 
+    #                                                 T_MAX, 
+    #                                                 DRAG_COEF, 
+    #                                                 wind, 
+    #                                                 GRAVITY, 
+    #                                                 COURT_LENGTH, 
+    #                                                 INITIAL_SPEED_GUESS).t_events[1][0] 
+    #                                     for x0, angle in zip(x0s.ravel(), angles.ravel())]).reshape(GRID_SHAPE)
+        
+    #     # Plot 3D
+    #     surface = mlab.surf(x0s, angles, times_to_opponent, color=color)
+    #     # surface = ax.plot_surface(x0s, angles, times_to_opponent, color=color, label=label)
+    
+    # mlab.axes(xlabel="Initial Position", ylabel="Launch Angle", zlabel="Time to Opponent")
+    # mlab.show()
 
+def time_difference_plot(n_samples: int, 
+                         tmin: float, 
+                         tmax: float, 
+                         drag_coef: float, 
+                         gravity: float, 
+                         court_length: float, 
+                         initial_speed_guess: float) -> None:
+    """Plot the difference in time-to-opponent for a variety of wind speeds
 
+    Args:
+        n_samples (int): The number of random samples to calculate time for
+        tmin (float): The start time of the ODE solver
+        tmax (float): The end time of the ODE solver
+        drag_coef (float): The drag coefficient
+        gravity (float): Gravitational acceleration
+        court_length (float): The length of the court
+        initial_speed_guess (float): The initial speed to guess when solving
+    """
+    
+    # Initialize with random parameters
+    wind_rand = np.random.uniform(0.0, 15.0, n_samples)
+    x0_rand = np.random.uniform(0.0, f2m(15.0), n_samples)
+    y0_rand = np.random.uniform(0.0, f2m(7.0), n_samples)
+    angle_rand = np.random.uniform(np.deg2rad(0.0), np.deg2rad(30.0), n_samples)
+    opponent_rand = np.random.uniform(f2m(29.0), f2m(44.0), n_samples)
+    
+    # Loop through and find times for all random params
+    times = np.zeros(n_samples)
+    for index, (wind, x0, y0, angle, opponent) in tqdm(enumerate(zip(wind_rand, x0_rand, y0_rand, angle_rand, opponent_rand))):
+        time_with_wind = solve_system(x0, 
+                                      angle, 
+                                      y0, 
+                                      opponent, 
+                                      tmin, 
+                                      tmax, 
+                                      drag_coef, 
+                                      wind, 
+                                      gravity, 
+                                      court_length, 
+                                      initial_speed_guess).t_events[1]
+        
+        time_against_wind = solve_system(x0, 
+                                         angle, 
+                                         y0, 
+                                         opponent, 
+                                         tmin, 
+                                         tmax, 
+                                         drag_coef, 
+                                         -wind, 
+                                         gravity, 
+                                         court_length, 
+                                         initial_speed_guess).t_events[1]
+        
+        times[index] = time_with_wind[0] - time_against_wind[0] if len(time_with_wind) > 0 and len(time_against_wind) > 0 else np.nan
+    
+    print(f"{np.count_nonzero(np.isnan(times))} NaN events")
+    
+    plt.hist2d(wind_rand[np.isfinite(times)], times[np.isfinite(times)], bins=20)
+    plt.xlabel("Wind Speed")
+    plt.ylabel("Time Difference Playing With Wind Versus Against Wind")
+    plt.title("Difference in Time-To-Opponent Playing With and\nAgainst Wind for Various Wind Speeds")
+    plt.colorbar()
+    plt.show()
 
 def solve_system(x0: float, 
                  angle: float, 
@@ -210,6 +271,30 @@ def m2f(metres: float) -> float:
     """
     
     return metres * 3.28084
+
+def mps2mph(metres_per_second: float) -> float:
+    """Convert a speed in metres per second to a speed in miles per hour
+
+    Args:
+        metres_per_second (float): Speed in m/s
+
+    Returns:
+        float: Speed in mph
+    """
+    
+    return metres_per_second * 2.23694
+
+def mph2mps(metres_per_second: float) -> float:
+    """Convert a speed in miles per hour to a speed in metres per second
+
+    Args:
+        metres_per_second (float): Speed in mph
+
+    Returns:
+        float: Speed in m/s
+    """
+    
+    return metres_per_second / 2.23694
 
 if __name__ == "__main__":
     main()
